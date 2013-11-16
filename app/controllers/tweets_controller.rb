21e7c9dbc078b36def70c8c1a98c7512d6851244
Tom Kim <tom.y.kim@gmail.com>
2013-11-16 14:46:01 -0800

    Fix the tweets controller to tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,27 +5,16 @@ class TweetsController < ApplicationController
 	end
 
 	def new
-		@user = User.find(params[:user_id])
 		@tweet = Tweet.new
 	end
 
 	def create
-			@tweet = Tweet.new(tweet_params)
-	    @tweet.user_id = current_user.id
-	    @tweet.save!
-	    flash[:info] = "Added a new tweet!"
-	    redirect_to tweet_path(:id)
+		@tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save!
+    flash.alert[:info] = "Added a new tweet!"
+    redirect_to tweet_path(:id)
 	end
-
-	# def create
-	# 	@tweet = Tweet.new(tweet_params)
- #    @user = User.find(params[:user_id])
- #    @tweet = @user.tweets.create(tweet_params)
- #    @tweet.user_id = current_user.id
- #    @tweet.save!
-	#   flash[:info] = "Added a new tweet!"
-	#   redirect_to user_path
-	# end
 
 	def show
 		 @tweet = Tweet.find(session[:user_id])
